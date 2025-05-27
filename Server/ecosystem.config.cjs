0d31b2a8b2292565221637276b17bfdd0e9723a5
ggh228 <33318713+ggh228@users.noreply.github.com>
2025-05-27 10:17:20 +0900

    라이브 회수제한 3

--- Server/ecosystem.config.cjs
+++ Server/ecosystem.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
                     'mysql://root:evgXHfGBMUxRdKvp@210.109.14.76:3306/abodeaddon-hair',
                 STORAGE_BUCKET: 'abodeaddon-hair',
                 SERVER_PORT: 443,
-                DAILY_IMAGE_GENERATION_LIMIT: 50, //검수 통과후 3장.
+                DAILY_IMAGE_GENERATION_LIMIT: 3, //검수 통과후 3장.
                 DAILY_IMAGE_GENERATION_RESET: 'false',
             },
         },
