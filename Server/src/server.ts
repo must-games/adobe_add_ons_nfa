7d01206a847d175b9d1e1370cf0f1b1c6449b7de
ggh228 <33318713+ggh228@users.noreply.github.com>
2025-09-30 17:37:46 +0900

    d

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -60,10 +60,10 @@ if (process.env.NODE_ENV === 'development') {
     })
 }
 
-//if (SERVER_TYPE == 'live') {
-//    app.use(express.static('html'))
-//} else if (process.env.NODE_ENV === 'development') {    
-//    app.use(express.static('html'))
-//}
+if (SERVER_TYPE == 'live') {
+   app.use(express.static('html'))
+} else if (process.env.NODE_ENV === 'development') {    
+   //app.use(express.static('html'))
+}
 
 export default app
